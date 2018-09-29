7c09979cc7d5a447d35bc123b39a7e34e139175d
Fabrice Perez <fabrice.perez@gmail.com>
2018-09-29 11:00:15 +0200

    Passage du site en total static. Un Cache-Control en private est setté automatiquement par OVH

--- htdocs/src/AppBundle/Controller/DefaultController.php
+++ htdocs/src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,6 +11,7 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Cache(public=true, smaxage=86400)
      */
     public function onePageAction(Request $request)
     {
